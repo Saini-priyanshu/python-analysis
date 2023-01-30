f9cd4b1eb5115f6a3f4bc8616ac049386021fc49
priyanshusaini123@gmail.com <priyanshusaini123@gmail.com>
2023-01-30 12:32:51 +0530

    operators

---
+++ hyp.py
@@ -0,0 +1,9 @@
+per = 12
+base = 5
+
+hyp =(per** + base**2)**.5
+print("PYHTOGOREAN THEOREM ")
+print(f"perpendicular is{per}")
+print(f"base is{base}")
+print("=="*10)
+print("hypotenuse is", hyp)
---
+++ si.py
@@ -0,0 +1,8 @@
+#SI calculator
+p=10000
+r=0.5
+t=10
+s=p*r*t/100
+print(f"principle is ${p}")
+print(f"rate is{r}%")
+print(f"")
